0d6569cb2fba9c25158a16655c6aba9cd6849e6e
Norman Mo <LTaoist6@gmail.com>
2012-04-28 23:28:07 +0800

    增加了g.py中的seq

--- chaofeng/g.py
+++ chaofeng/g.py
@@ -21,9 +21,13 @@ mark = Proxyer()
 # Normal file map 
 f_map = Proxyer()
 
+@f_map('.seq')
+def load_seq(f):
+    return [ x[:-1] for x in f.readlines()]
+
 @f_map('.txt')
 def load_str(f):
-    return u'\r'.join(f.readlines())
+    return u'\r'.join(f.readlines())[:-1]
 
 @f_map('.tpl')
 def load_templete(f):
@@ -50,7 +54,6 @@ class StaticProxyer(Proxyer):
     Load the static resouce.
     '''
     def load(self,path='./static/',file_map=None,encoding="utf8",mode="r"):
-        print '.'*20
         if file_map == None :
             file_map = f_map
         for root,dirs,files in os_walk(path):
